66531f2f3efeb19ed2e6fede543efcbd449fb27a
Idan Frimark <idanf@cisco.com>
2021-10-07 08:48:18 +0300

    add license header and fix return values

--- backend/pkg/rest/apiinventory_delete_spec.go
+++ backend/pkg/rest/apiinventory_delete_spec.go
@@ -1,3 +1,18 @@
+// Copyright © 2021 Cisco Systems, Inc. and its affiliates.
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package rest
 
 import (
@@ -15,17 +30,17 @@ func (s *Server) DeleteAPIInventoryAPIIDSpecsProvidedSpec(params operations.Dele
 	apiInfo := &database.APIInfo{}
 
 	if err := database.GetAPIInventoryTable().First(&apiInfo, params.APIID).Error; err != nil {
-		log.Errorf("Failed to get APIInventory table with api id: %v. %v", params.APIID, err)
-		return operations.NewPutAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
+		log.Errorf("Failed to get API info. id=%v: %v", params.APIID, err)
+		return operations.NewDeleteAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
 	}
 
 	if err := s.speculator.UnsetProvidedSpec(speculator.GetSpecKey(apiInfo.Name, strconv.Itoa(int(apiInfo.Port)))); err != nil {
 		log.Errorf("Failed to unset provided spec. %v", err)
-		return operations.NewPutAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
+		return operations.NewDeleteAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
 	}
 	if err := database.DeleteProvidedAPISpec(params.APIID); err != nil {
 		log.Errorf("Failed to delete provided spec with api id: %v from DB. %v", params.APIID, err)
-		return operations.NewPutAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
+		return operations.NewDeleteAPIInventoryAPIIDSpecsProvidedSpecDefault(http.StatusInternalServerError)
 	}
 
 	return operations.NewDeleteAPIInventoryAPIIDSpecsProvidedSpecOK().WithPayload(&models.SuccessResponse{
@@ -37,17 +52,17 @@ func (s *Server) DeleteAPIInventoryAPIIDSpecsReconstructedSpec(params operations
 	apiInfo := &database.APIInfo{}
 
 	if err := database.GetAPIInventoryTable().First(&apiInfo, params.APIID).Error; err != nil {
-		log.Errorf("Failed to get APIInventory table with api id: %v. %v", params.APIID, err)
+		log.Errorf("Failed to get API info. id=%v: %v", params.APIID, err)
 		return operations.NewDeleteAPIInventoryAPIIDSpecsReconstructedSpecDefault(http.StatusInternalServerError)
 	}
 
 	if err := s.speculator.UnsetApprovedSpec(speculator.GetSpecKey(apiInfo.Name, strconv.Itoa(int(apiInfo.Port)))); err != nil {
-		log.Errorf("Failed to unset provided spec. %v", err)
+		log.Errorf("Failed to unset reconstructed spec. %v", err)
 		return operations.NewDeleteAPIInventoryAPIIDSpecsReconstructedSpecDefault(http.StatusInternalServerError)
 	}
 
 	if err := database.DeleteApprovedAPISpec(params.APIID); err != nil {
-		log.Errorf("Failed to delete provided spec with api id: %v from DB. %v", params.APIID, err)
+		log.Errorf("Failed to delete reconstructed spec with api id: %v from DB. %v", params.APIID, err)
 		return operations.NewDeleteAPIInventoryAPIIDSpecsReconstructedSpecDefault(http.StatusInternalServerError)
 	}
 
