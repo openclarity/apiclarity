d47af69bc73fd98c1eaad1119b8e1657603ac8fe
Idan Frimark <idanf@cisco.com>
2021-12-22 09:22:52 +0200

    remove the deploy script

--- plugins/taper/deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-UpstreamTelemetryHostName="${UPSTREAM_TELEMETRY_HOST_NAME:-apiclarity-apiclarity.apiclarity:9000}"
-NamespacesToTap="${NAMESPACES_TO_TAP:-default}"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cat "${DIR}/gopacket-ds.yml" | sed "s/{{NAMESPACES_TO_TAP}}/\"$NamespacesToTap\"/g" | sed "s/{{UPSTREAM_TELEMETRY_HOST_NAME}}/$UpstreamTelemetryHostName/g" | kubectl -n apiclarity apply -f -
