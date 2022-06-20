e39f605ad7587ef7f0c4572dc5245a65db8e042b
Alessandro Duminuco <aduminuc@cisco.com>
2022-06-20 23:29:17 +0200

    Fix notification order generation

--- api3/generate.sh
+++ api3/generate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash	
 
-
 (cd ../tools/spec-aggregator && go run main.go)
 (cd ./common && go generate)
 (cd ./global && go generate)
@@ -9,8 +8,9 @@
 if [[ $1 == "--verify" ]]  ; then
     diffs=$(git status --porcelain)
     if [[ ${diffs} != "" ]]; then
-        echo "Verification Failed: this script was not run on the latest version of the specs"
+        echo "Verification Failed: the spec aggregation and the code generation was not run on the latest version of the specs"
         git diff
+        echo "Please run `make api3` and commit again"
         exit 1
     fi
 fi
