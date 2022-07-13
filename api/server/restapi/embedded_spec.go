be2abd95d5e8eaf608188cd64ed08674311d1235
Erez Fishhimer <fishkerez.github@gmail.com>
2022-07-13 12:53:14 +0300

    add spec timestamp to diffs notification

--- api/server/restapi/embedded_spec.go
+++ api/server/restapi/embedded_spec.go
@@ -804,7 +804,7 @@ func init() {
         "specdiffs",
         "traceanalyzer",
         "bfla",
-        "spec_diffs",
+        "differ",
         "fuzzer"
       ]
     },
@@ -3007,7 +3007,7 @@ func init() {
         "specdiffs",
         "traceanalyzer",
         "bfla",
-        "spec_diffs",
+        "differ",
         "fuzzer"
       ]
     },
