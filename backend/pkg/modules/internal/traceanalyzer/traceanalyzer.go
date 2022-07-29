00d5d5b2190a2d18279d95dd74596406f45ebc7a
Julien Barbot <jubarbot@cisco.com>
2022-07-29 11:12:39 +0200

    TA: Store list of findings per type
    The api_annotations has a unique index
    on (`module_name`,`api_id`,`name`), meaning there can be only on api
    finding type per (module_name, api_id).
    
    But we have multiple findings with the same name, which relates to
    different path.
    
    That's why each row correspond to one type of finding, but contains a
    list of findings for each different path.

--- backend/pkg/modules/internal/traceanalyzer/traceanalyzer.go
+++ backend/pkg/modules/internal/traceanalyzer/traceanalyzer.go
@@ -347,20 +347,30 @@ func fromCoreEventAnnotations(coreAnns []*core.Annotation) (anns []utils.TraceAn
 }
 
 func (p *traceAnalyzer) toCoreAPIAnnotations(anns []utils.TraceAnalyzerAPIAnnotation, redacted bool) (coreAnnotations []core.Annotation) {
+	// In order to create Core Annotations, we need to group APIAnnotations by Name of annotations
+	groupedAnns := map[string][]utils.TraceAnalyzerAPIAnnotation{}
 	for _, a := range anns {
 		if redacted {
 			a = a.Redacted()
 		}
-		annotation, err := json.Marshal(a)
+		groupedAnns[a.Name()] = append(groupedAnns[a.Name()], a)
+	}
+	for annName, anns := range groupedAnns {
+		annotation, err := json.Marshal(anns)
 		if err != nil {
 			log.Errorf("unable to serialize annotation: %s", err)
 		}
-		coreAnnotations = append(coreAnnotations, core.Annotation{Name: a.Name(), Annotation: annotation})
+		coreAnnotations = append(coreAnnotations, core.Annotation{Name: annName, Annotation: annotation})
 	}
 	return coreAnnotations
 }
 
-func fromCoreAPIAnnotation(coreAnn *core.Annotation) (ann utils.TraceAnalyzerAPIAnnotation, err error) {
+func fromCoreAPIAnnotation(coreAnn *core.Annotation) (anns []utils.TraceAnalyzerAPIAnnotation, err error) {
+	var rawAnnotations []json.RawMessage
+	if err := json.Unmarshal(coreAnn.Annotation, &rawAnnotations); err != nil {
+		return anns, fmt.Errorf("unable to convert API Annotation to DB representation: %w", err)
+	}
+
 	var a utils.TraceAnalyzerAPIAnnotation
 	switch coreAnn.Name {
 	case weakbasicauth.KindShortPassword:
@@ -398,20 +408,24 @@ func fromCoreAPIAnnotation(coreAnn *core.Annotation) (ann utils.TraceAnalyzerAPI
 		return nil, fmt.Errorf("unknown annotation '%s'", coreAnn.Name)
 	}
 
-	err = json.Unmarshal(coreAnn.Annotation, a)
-	if err != nil {
-		return a, fmt.Errorf("unable to convert API Annotation to DB representation: %w", err)
+	for _, rawJSON := range rawAnnotations {
+		if err := json.Unmarshal(rawJSON, a); err != nil {
+			log.Errorf("Unable to unmarshal one of the %s annotations", coreAnn.Name)
+			log.Debugf("Unable to unmarshal annotation of type %s %s", coreAnn.Name, string(rawJSON))
+		} else {
+			anns = append(anns, a)
+		}
 	}
-	return a, nil
+	return
 }
 
 func fromCoreAPIAnnotations(coreAnns []*core.Annotation) (anns []utils.TraceAnalyzerAPIAnnotation) {
 	for _, coreAnn := range coreAnns {
-		taAnn, err := fromCoreAPIAnnotation(coreAnn)
+		taAnns, err := fromCoreAPIAnnotation(coreAnn)
 		if err != nil {
 			log.Errorf("Unable to understand annotation: %v", err)
 		} else {
-			anns = append(anns, taAnn)
+			anns = append(anns, taAnns...)
 		}
 	}
 
