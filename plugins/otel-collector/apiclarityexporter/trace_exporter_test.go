d805c578345a560e2f7710761ebb3fe99751249c
Jeff Napper (jenapper) <jenapper@cisco.com>
2022-09-20 15:40:54 +0200

    Fixes for tests.

--- plugins/otel-collector/apiclarityexporter/trace_exporter_test.go
+++ plugins/otel-collector/apiclarityexporter/trace_exporter_test.go
@@ -17,29 +17,20 @@ package apiclarityexporter
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componenttest"
-	otelconfig "go.opentelemetry.io/collector/config"
 	"go.opentelemetry.io/collector/config/confighttp"
-	"go.opentelemetry.io/collector/consumer/consumererror"
-	"go.opentelemetry.io/collector/exporter/exporterhelper"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 
 	servermodels "github.com/openclarity/apiclarity/plugins/api/server/models"
@@ -173,190 +164,5 @@ func startAndCleanup(t *testing.T, cmp component.Component) {
 	require.NoError(t, cmp.Start(context.Background(), componenttest.NewNopHost()))
 	t.Cleanup(func() {
 		require.NoError(t, cmp.Shutdown(context.Background()))
-	})
-}
-
-func TestErrorResponses(t *testing.T) {
-	addr := GetAvailableLocalAddress(t, "tcp")
-	errMsgPrefix := fmt.Sprintf("error exporting items, request to http://%s/v1/traces responded with HTTP Status Code ", addr)
-
-	tests := []struct {
-		name           string
-		responseStatus int
-		responseBody   *status.Status
-		err            error
-		isPermErr      bool
-		headers        map[string]string
-	}{
-		{
-			name:           "400",
-			responseStatus: http.StatusBadRequest,
-			responseBody:   status.New(codes.InvalidArgument, "Bad field"),
-			isPermErr:      true,
-		},
-		{
-			name:           "404",
-			responseStatus: http.StatusNotFound,
-			err:            errors.New(errMsgPrefix + "404"),
-		},
-		{
-			name:           "419",
-			responseStatus: http.StatusTooManyRequests,
-			responseBody:   status.New(codes.InvalidArgument, "Quota exceeded"),
-			err: exporterhelper.NewThrottleRetry(
-				errors.New(errMsgPrefix+"429, Message=Quota exceeded, Details=[]"),
-				time.Duration(0)*time.Second),
-		},
-		{
-			name:           "503",
-			responseStatus: http.StatusServiceUnavailable,
-			responseBody:   status.New(codes.InvalidArgument, "Server overloaded"),
-			err: exporterhelper.NewThrottleRetry(
-				errors.New(errMsgPrefix+"503, Message=Server overloaded, Details=[]"),
-				time.Duration(0)*time.Second),
-		},
-		{
-			name:           "503-Retry-After",
-			responseStatus: http.StatusServiceUnavailable,
-			responseBody:   status.New(codes.InvalidArgument, "Server overloaded"),
-			headers:        map[string]string{"Retry-After": "30"},
-			err: exporterhelper.NewThrottleRetry(
-				errors.New(errMsgPrefix+"503, Message=Server overloaded, Details=[]"),
-				time.Duration(30)*time.Second),
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			mux := http.NewServeMux()
-			mux.HandleFunc("/v1/traces", func(writer http.ResponseWriter, request *http.Request) {
-				for k, v := range test.headers {
-					writer.Header().Add(k, v)
-				}
-				writer.WriteHeader(test.responseStatus)
-				if test.responseBody != nil {
-					msg, err := proto.Marshal(test.responseBody.Proto())
-					require.NoError(t, err)
-					_, err = writer.Write(msg)
-					require.NoError(t, err)
-				}
-			})
-			srv := http.Server{
-				Addr:    addr,
-				Handler: mux,
-			}
-			ln, err := net.Listen("tcp", addr)
-			require.NoError(t, err)
-			go func() {
-				_ = srv.Serve(ln)
-			}()
-
-			cfg := &Config{
-				ExporterSettings: otelconfig.NewExporterSettings(otelconfig.NewComponentID(typeStr)),
-				// Create without QueueSettings and RetrySettings so that ConsumeTraces
-				// returns the errors that we want to check immediately.
-			}
-			set := componenttest.NewNopExporterCreateSettings()
-			set.Logger, err = zap.NewDevelopment()
-			require.NoError(t, err)
-			exp, err := CreateTracesExporter(context.Background(), set, cfg)
-			require.NoError(t, err)
-
-			// start the exporter
-			err = exp.Start(context.Background(), componenttest.NewNopHost())
-			require.NoError(t, err)
-			t.Cleanup(func() {
-				require.NoError(t, exp.Shutdown(context.Background()))
-			})
-
-			// generate traces
-			traces := ptrace.NewTraces()
-			err = exp.ConsumeTraces(context.Background(), traces)
-			assert.Error(t, err)
-
-			if test.isPermErr {
-				assert.True(t, consumererror.IsPermanent(err))
-			} else {
-				assert.EqualValues(t, test.err, err)
-			}
-
-			srv.Close()
-		})
-	}
-}
-
-func TestUserAgent(t *testing.T) {
-	var err error
-	addr := GetAvailableLocalAddress(t, "tcp")
-	set := componenttest.NewNopExporterCreateSettings()
-	set.BuildInfo.Description = "Collector"
-	set.BuildInfo.Version = "1.2.3test"
-	set.Logger, err = zap.NewDevelopment()
-	require.NoError(t, err)
-
-	tests := []struct {
-		name       string
-		headers    map[string]string
-		expectedUA string
-	}{
-		{
-			name:       "default_user_agent",
-			expectedUA: "Collector/1.2.3test",
-		},
-		{
-			name:       "custom_user_agent",
-			headers:    map[string]string{"User-Agent": "My Custom Agent"},
-			expectedUA: "My Custom Agent",
-		},
-		{
-			name:       "custom_user_agent_lowercase",
-			headers:    map[string]string{"user-agent": "My Custom Agent"},
-			expectedUA: "My Custom Agent",
-		},
-	}
-
-	t.Run("traces", func(t *testing.T) {
-		for _, test := range tests {
-			t.Run(test.name, func(t *testing.T) {
-				mux := http.NewServeMux()
-				mux.HandleFunc("/v1/traces", func(writer http.ResponseWriter, request *http.Request) {
-					assert.Contains(t, request.Header.Get("user-agent"), test.expectedUA)
-					writer.WriteHeader(200)
-				})
-				srv := http.Server{
-					Addr:    addr,
-					Handler: mux,
-				}
-				ln, err := net.Listen("tcp", addr)
-				require.NoError(t, err)
-				go func() {
-					_ = srv.Serve(ln)
-				}()
-
-				factory := NewFactory()
-				cfg := factory.CreateDefaultConfig().(*Config)
-				cfg.HTTPClientSettings = confighttp.HTTPClientSettings{
-					Headers:  test.headers,
-					Endpoint: fmt.Sprintf("http://%s", addr),
-				}
-
-				exp, err := CreateTracesExporter(context.Background(), set, cfg)
-				require.NoError(t, err)
-
-				// start the exporter
-				err = exp.Start(context.Background(), componenttest.NewNopHost())
-				require.NoError(t, err)
-				t.Cleanup(func() {
-					require.NoError(t, exp.Shutdown(context.Background()))
-				})
-
-				// generate data
-				traces := ptrace.NewTraces()
-				err = exp.ConsumeTraces(context.Background(), traces)
-				require.NoError(t, err)
-
-				srv.Close()
-			})
-		}
 	})
 }
