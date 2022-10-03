981aa7bddae49383bf32647665fe08f300269d80
Michael Ryan <michrya2@cisco.com>
2022-10-03 14:59:22 -0400

    Core Component Updates
    - updates to core components to support BFLA and Fuzzer.

--- ui/src/components/ListDisplay/index.js
+++ ui/src/components/ListDisplay/index.js
@@ -7,16 +7,19 @@ import './list-display.scss';
 const ListDisplay = ({items, itemDisplay: ItemDisplay, selectedId, onSelect}) => (
     <div className="list-display-wrapper">
         {
-            items.map(item => (
-                <div
-                    key={item.id}
-                    className={classnames("list-display-item-wrapper", {selected: item.id === selectedId})}
-                    onClick={() => onSelect(item)}
-                >
-                    <div className="list-display-item"><ItemDisplay {...item} /></div>
-                    <Arrow name={ARROW_NAMES.RIGHT} small />
-                </div>
-            ))
+            items.map(item => {
+                const {disabled=false} = item;
+                return (
+                    <div
+                        key={item.id}
+                        className={classnames("list-display-item-wrapper", { selected: item.id === selectedId }, {disabled})}
+                        onClick={disabled ? undefined : () => onSelect(item)}
+                    >
+                        <div className="list-display-item"><ItemDisplay {...item} /></div>
+                        <Arrow name={ARROW_NAMES.RIGHT} small />
+                    </div>
+                );
+            })
         }
     </div>
 );
--- ui/src/utils/utils.js
+++ ui/src/utils/utils.js
@@ -1,16 +1,17 @@
 import moment from 'moment';
+import COLORS from 'utils/scss_variables.module.scss';
 
 export const formatDateBy = (date, format) => !!date ? moment(date).format(format): "";
 export const formatDate = (date) => formatDateBy(date, "MMM Do, YYYY HH:mm:ss");
 
 export const SYSTEM_RISKS = {
-    CRITICAL: {value: "CRITICAL", label: "Critical"},
-    HIGH: {value: "HIGH", label: "High"},
-    MEDIUM: {value: "MEDIUM", label: "Medium"},
-    LOW: {value: "LOW", label: "Low"},
+    CRITICAL: {value: "CRITICAL", label: "Critical", color: COLORS["color-risk-critical"]},
+    HIGH: {value: "HIGH", label: "High", color: COLORS["color-risk-high"]},
+    MEDIUM: {value: "MEDIUM", label: "Medium", color: COLORS["color-risk-medium"]},
+    LOW: {value: "LOW", label: "Low", color: COLORS["color-risk-low"]},
     UNKNOWN: {value: "UNKNOWN", label: "Unknown"},
     NO_RISK: {value: "NO_RISK", label: "No known risk"},
     APPROVED: {value: "APPROVED", label: "Approved"},
     NEUTRAL: {value: "NEUTRAL", label: "Neutral"},
-    INFO: {value: "INFO", label: "Info"}
+    INFO: {value: "INFO", label: "Info", color: COLORS["color-risk-info"]}
 };
