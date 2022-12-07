f81bdfdf028875aea3c586db7caff890720a1893
Julien Barbot <jubarbot@cisco.com>
2022-12-07 11:27:03 +0100

    Add TraceSourceID to APIinfo in specdiff

--- backend/pkg/modules/internal/spec_differ/diffs_sender.go
+++ backend/pkg/modules/internal/spec_differ/diffs_sender.go
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint: revive,stylecheck
+// nolint: revive,stylecheck
 package spec_differ
 
 import (
@@ -126,6 +126,7 @@ func convertAPIInfo(apiInfo *database.APIInfo) common.ApiInfoWithType {
 		Id:                   &id,
 		Name:                 &apiInfo.Name,
 		Port:                 &port,
+		TraceSourceId:        &apiInfo.TraceSource.UID,
 	}
 }
 
